d0692a25967850c2a527c7f8bedb767a2caed0d7
John McFarlane <fixed-point@john.mcfarlane.name>
2016-04-04 01:47:14 -0700

    updated docs and papers

--- search/typedefs_1.js
+++ search/typedefs_1.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['make_5ffixed',['make_fixed',['../namespacesg14.html#adfab7b3dbdbbaa871587d31890bf3b67',1,'sg14']]],
+  ['make_5ffixed',['make_fixed',['../namespacesg14.html#a105fb7fedf4da94edf340e42786d997e',1,'sg14']]],
   ['make_5fufixed',['make_ufixed',['../namespacesg14.html#a5103178fc8b5447099bc9bf637e5cb1c',1,'sg14']]]
 ];
