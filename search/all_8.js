3e0ed15ec59e6cdc1f259f737fdb0e9c242d7af8
John McFarlane <git@john.mcfarlane.name>
2019-04-06 09:14:26 +0100

    Mark fixed_point as deprecated

--- search/all_8.js
+++ search/all_8.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['operator_20bool',['operator bool',['../classsg14_1_1fixed__point.html#a291beee495f779d277a2f0fe33e3586f',1,'sg14::fixed_point']]],
+  ['enable_5fif_3c_20std_3a_3ais_5fconvertible_3c_20rep_2c_20bool_20_3e_3a_3avalue_2c_20bool_20_3e',['enable_if&lt; std::is_convertible&lt; Rep, bool &gt;::value, bool &gt;',['../classsg14_1_1fixed__point.html#a43d1637ff121db0151b725be26061cb7',1,'sg14::fixed_point']]],
   ['operator_20s',['operator S',['../classsg14_1_1elastic__integer.html#a398961aa9a7a64b109a6de9134e9f320',1,'sg14::elastic_integer::operator S()'],['../classsg14_1_1fixed__point.html#a0d44142820943030ba0a8bc748742846',1,'sg14::fixed_point::operator S() const'],['../classsg14_1_1fixed__point.html#a0d44142820943030ba0a8bc748742846',1,'sg14::fixed_point::operator S() const']]],
   ['operator_3d',['operator=',['../classsg14_1_1elastic__integer.html#a4140f855848ca5466cf33dbf35c6e9cb',1,'sg14::elastic_integer::operator=()'],['../classsg14_1_1fixed__point.html#a83257fe0a99cac804a3e8aaf6f58a081',1,'sg14::fixed_point::operator=(S s)'],['../classsg14_1_1fixed__point.html#a83257fe0a99cac804a3e8aaf6f58a081',1,'sg14::fixed_point::operator=(S s)'],['../classsg14_1_1fixed__point.html#a6b0dd66ac124a2ea1dc7bd8c99e213c0',1,'sg14::fixed_point::operator=(const fixed_point&lt; FromRep, FromExponent &gt; &amp;rhs)']]]
 ];
